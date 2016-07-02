4b6c40398c7373201d8f54f98ac1d613392cfd77
vivinantony <vivinantony@gmail.com>
2016-07-02 09:49:28 +0530

    added discussion page and added show more topics for blog page

--- www/js/blogCtrl.js
+++ www/js/blogCtrl.js
@@ -1,12 +1,13 @@
 angular.module('tltApp')
 
-.controller('BlogCtrl', function($scope, $http, $state, $timeout, $ionicLoading) {
+.controller('BlogCtrl', function($scope, $http, $state, $timeout, $ionicLoading, $ionicScrollDelegate) {
     $ionicLoading.show();
 
     var key = "AIzaSyCODXphcNLhOyx83sdCWMTeDD9LmO3_QBY";
     var api = "https://www.googleapis.com/blogger/v3/blogs/8625939250791601558/posts?fetchBodies=false&key=" + key;
     $http.get(api).success(function(res) {
         $scope.datas = res.items;
+        $scope.nextset = res;
         // localStorage.setItem('postTopics', JSON.stringify($scope.datas));
     }).catch(function(err) {
         console.log("Error BlogCtrl: " + err);
@@ -17,6 +18,21 @@ angular.module('tltApp')
     // $scope.saved = localStorage.getItem('postTopics');
     // $scope.datas = (localStorage.getItem('postTopics') !== null) ? JSON.parse($scope.saved) : [];
     // localStorage.setItem('postTopics', JSON.stringify($scope.datas));
+
+    $scope.nextPage = function(token) {
+        $ionicLoading.show();
+        var api = "https://www.googleapis.com/blogger/v3/blogs/8625939250791601558/posts?pageToken=" + token + "&fetchBodies=false&key=" + key;
+        $http.get(api).success(function(res) {
+            $scope.datas = res.items;
+            $scope.nextset = res;
+            // localStorage.setItem('postTopics', JSON.stringify($scope.datas));
+        }).catch(function(err) {
+            console.log("Error BlogCtrl: " + err);
+        }).finally(function() {
+            $ionicLoading.hide();
+            $ionicScrollDelegate.scrollTop(true);
+        });
+    };
 
     $scope.search = function(query) {
         $ionicLoading.show();
